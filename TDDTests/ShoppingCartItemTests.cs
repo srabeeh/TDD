45134b022cc6ae469b714763b15e9a58b558ed93
srabeeh <srabeeh@larci.com>
2016-05-16 18:47:18 -0600

    Cart item qty test

--- TDDTests/ShoppingCartItemTests.cs
+++ TDDTests/ShoppingCartItemTests.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TDDTests
 {
-    class ShoppingCartItemTests
+    [TestClass]
+    public class ShoppingCartItemTests
     {
+        // setup
+        private int _qty = 5;
+
+        [TestMethod]
+        public void CartItemQuantity()
+        {
+            var Item = new CartItem(_qty);
+
+            Assert.AreEqual(_qty, Item.Quantity);
+        }
+    }
+
+    internal class CartItem
+    {
+        public int Quantity { get; set; }
+
+        public CartItem(int _quantity)
+        {
+            this.Quantity = _quantity;
+        }
+
     }
 }
