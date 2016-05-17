1a5af547c7623c497e1d819bece9b4b3f3591b99
srabeeh <srabeeh@larci.com>
2016-05-16 22:00:08 -0600

    Cart with zero items test

---
+++ TDDTests/CartTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShoppingCart;
+
+namespace TDDTests
+{
+    [TestClass]
+    class CartTests
+    {
+        [TestMethod]
+        public void CartCanContainZeroItems()
+        {
+            var cart = new Cart();
+
+            Assert.AreEqual(cart.Items.Count, 0);
+        }
+    }
+
+
+}
