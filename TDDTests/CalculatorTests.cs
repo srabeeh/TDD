c5805dd4884e28810a8fd6e19c75b8bd0a58c20a
srabeeh <srabeeh@larci.com>
2016-05-16 18:12:26 -0600

    Data driven tests

--- TDDTests/CalculatorTests.cs
+++ TDDTests/CalculatorTests.cs
@@ -7,15 +7,41 @@ namespace TDDTests
     [TestClass]
     public class CalculatorTests
     {
-        
+        private TestContext _testContextInstance;
+        public TestContext TestContext
+        {
+            get { return _testContextInstance; }
+            set { _testContextInstance = value; }
+        }
+
+        /// <summary>
+        /// Data-Driven Test
+        /// WTH is up with the data-driven tests documentation? Ancient
+        /// </summary>
+        [TestMethod]
+        [DataSource(@"Provider=Microsoft.SqlServerCe.Client.4.0; Data Source=C:\src\TDD\TddPractice\TDD_BDD_Practice\TDDTests\Data\CalcData.sdf;", "Numbers")]
+        public void AddTwoIntegersDataDriven()
+        {
+            int add1 = Convert.ToInt32(TestContext.DataRow["FirstNumber"]);
+            int add2 = Convert.ToInt32(TestContext.DataRow["SecondNumber"]);
+            int expected = Convert.ToInt32(TestContext.DataRow["Sum"]);
+
+            int result = Calculator<int>.Add(add1, add2);
+
+            Assert.AreEqual(expected, result);
+        }
+
 
         [TestMethod]
         public void AddTwoIntegers()
         {
-            int answer = Calculator<int>.Add(5, 5);
-            int expected = 10;
+            int add1 = 5;
+            int add2 = 50;
 
-            Assert.AreEqual(expected,answer);
+            int expected = 55;
+            int result = Calculator<int>.Add(add1, add2);
+
+            Assert.AreEqual(expected, result);
         }
 
         [TestMethod]
